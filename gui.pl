079738d5f52795497e18901b59a6afae89ec199f
Jan Wielemaker <jan@swi-prolog.org>
2024-10-10 21:07:48 +0200

    Round floats in CSV

--- gui.pl
+++ gui.pl
@@ -227,10 +227,66 @@ download_csv(SHA1, _Request) :-
     saved(SHA1, Model, Options),
     simulate(string(Model), Series, Options),
     Series = [First|_],
-    dict_keys(First, Keys),
-    dicts_to_compounds(Series, Keys, dict_fill(-), Compounds),
+    dict_keys(First, Keys0),
+    order_keys(Keys0, Keys),
+    dicts_to_compounds(Series, Keys, dict_fill(-), Compounds0),
+    maplist(round_float_row(4), Compounds0, Compounds),
     format('Content-type: text/csv~n~n'),
-    Title =.. [row|Keys],
+    option(id_mapping(IdMapping), Options, _{}),
+    maplist(key_label(IdMapping), Keys, Labels),
+    Title =.. [row|Labels],
     csv_write_stream(current_output, [Title|Compounds],
                      [ separator(0',)]).
 
+key_label(_, t, "Time") :-
+    !.
+key_label(IdMapping, Key, Label) :-
+    format(atom(Label), '~w', [IdMapping.get(Key)]),
+    !.
+key_label(_, Key, Key).
+
+order_keys(Keys0, Keys) :-
+    map_list_to_pairs(csv_column_rank, Keys0, Pairs),
+    keysort(Pairs, PairsS),
+    pairs_values(PairsS, Keys).
+
+csv_column_rank(t, 0).
+csv_column_rank(Key, 1) :- sub_atom(Key, _, _, _, number_of), !.
+csv_column_rank(Key, 2) :- sub_atom(Key, _, _, _, growth), !.
+csv_column_rank(_,   3).
+
+round_float_row(N, Row0, Row) :-
+    same_functor(Row0, Row),
+    functor(Row0, _, Arity),
+    round_float_cols(1, Arity, N, Row0, Row).
+
+round_float_cols(I, Arity, N, Row0, Row) :-
+    I =< Arity,
+    !,
+    arg(I, Row0, F0),
+    arg(I, Row, F),
+    (   float(F0)
+    ->  round_float(N, F0, F)
+    ;   F = F0
+    ),
+    I2 is I+1,
+    round_float_cols(I2, Arity, N, Row0, Row).
+round_float_cols(_, _, _, _, _).
+
+round_float(_Decimals, Value, Rounded) :-
+    Value =:= 0,
+    !,
+    Rounded = 0.0.
+round_float(Decimals, Value, Rounded) :-
+    Value < 0,
+    !,
+    round_float(Decimals, -Value, MinRounded),
+    Rounded is -MinRounded.
+round_float(Decimals, Value, Rounded) :-
+    FracDecimals is Decimals-floor(log10(Value))-1,
+    (   FracDecimals > 0
+    ->  Mult is 10.0^FracDecimals,
+        Rounded is round(Value*Mult)/Mult
+    ;   Mult is 10.0^(-FracDecimals),
+        Rounded is round(Value/Mult)*Mult
+    ).
