d384f7f33979aa4acf39724e0100c6960fff2ecc
Jan Wielemaker <jan@swi-prolog.org>
2024-10-21 14:37:54 +0200

    Sync signs with garp

--- numeric/force-and-motion.pl
+++ numeric/force-and-motion.pl
@@ -3,16 +3,16 @@ velocity(object) := velocity(object) + acceleration(object) * dt.
 acceleration(object) := net_force(object)/m.
 gravity(object) := m*g.
 air_resistance(object) := 0.5*cw*rho*opp*velocity(object)^2.
-net_force(object) := gravity(object) + air_resistance(object).
+net_force(object) := gravity(object) - air_resistance(object).
 
 t := t + dt.
 t := 0.
 dt := 0.01.
 
-distance(object) := 70.
+distance(object) := 0.
 velocity(object) := 0.
 m := 0.270.
-g := -9.81.
+g := 9.81.
 opp := 0.0347.
 rho := 1.293.
 cw := 0.5.
