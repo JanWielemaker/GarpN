6a7a74e156e283ba8268baaf1af308e7764ae5ae
Jan Wielemaker <jan@swi-prolog.org>
2024-11-19 16:36:14 +0100

    WIP improving equation support.

--- equations.pl
+++ equations.pl
@@ -5,6 +5,7 @@
 :- use_module(library(http/html_write)).
 :- use_module(library(http/js_write)).
 :- use_module(library(http/html_head)).
+:- use_module(library(dcg/basics)).
 
 :- html_resource(mathlive,
                  [ virtual(true),
@@ -47,8 +48,10 @@ eq_to_mathjax(Left := Right) ==>
 
 quantity(Q), compound(Q), Q =.. [A,E] ==>
     format('\\prop{~w}{~w}', [A,E]).
-quantity(Q) ==>
-    format('\\unit{~w}', [Q]).
+quantity(Q), atom(Q) ==>
+    format('\\variable{~w}', [Q]).
+quantity(Q), number(Q) ==>
+    format('\\placeholder[c]{~w}', [Q]).
 
 expression(A + B) ==> expression(A), " + ", expression(B).
 expression(A - B) ==> expression(A), " - ", expression(B).
@@ -58,3 +61,58 @@ expression(Q), ground(Q) ==> quantity(Q).
 
 format(Fmt, Args, Head, Tail) :-
     format(codes(Head, Tail), Fmt, Args).
+
+
+		 /*******************************
+		 *            PARSE		*
+		 *******************************/
+
+parse_latex(String, Formula) :-
+    string_codes(String, Codes),
+    phrase(latex(eos, Formula), Codes).
+
+latex(End, List) -->
+    is_end(End),
+    !,
+    { List = [] }.
+latex(End, [H|T]) -->
+    latex_1(End, H),
+    latex(End, T).
+
+latex_1(_, Token) --> latex_cmd(Token), !.
+latex_1(End, String) --> string(Codes), ends_string(End), !, {string_codes(String, Codes)}.
+
+ends_string(End) --> is_end(End), !.
+ends_string(_), "\\" --> "\\", !.
+ends_string(_), "{" --> "{", !.
+ends_string(_), "}" --> "}", !.
+
+is_end(eos) --> eos, !.
+is_end(String), [C]--> [C], {string_code(_, String, C)}, !.
+
+latex_cmd(Command) -->
+    "\\", ltx_name(Name), ltx_args(Name, Args),
+    { compound_name_arguments(Command, Name, Args) }.
+
+ltx_name(Name) -->
+    csym(Name).
+
+ltx_args(prop, Args) ==> ltx_nargs(2, Args).
+ltx_args(_,    Args) ==> curl_args(Args).
+
+ltx_nargs(0, []) --> !.
+ltx_nargs(N, [H|T]) --> ltx_arg(H), {N1 is N-1}, ltx_nargs(N1, T).
+
+ltx_arg(Arg) --> curl_arg(Arg), !.
+
+curl_args([H|T]) -->
+    peek("{"),
+    !,
+    curl_arg(H),
+    curl_args(T).
+curl_args([]) -->
+    [].
+
+curl_arg(Arg) --> "{", !, latex("}", Arg), "}".
+
+peek(C), [C] --> [C].
--- web/equations.js
+++ web/equations.js
@@ -8,8 +8,18 @@ function ml_init()
     const mf = eq.querySelector("math-field");
     mf.macros = {
       ... mf.macros,
-      prop: '\\,\\text{#1}^{#2}\\,{}',
-      unit: '\\text{#1}',
+      prop:     '\\,\\text{#1}^{#2}\\,{}',
+      variable: '\\text{#1}'
     };
   }
+}
+
+function ml_value()
+{ const val = [];
+  const eql = document.getElementById("equations");
+  for(eq of eql.children) {
+    const mf = eq.querySelector("math-field");
+    val.push(mf.getValue('math-json'));
+  }
+  return val;
 }
