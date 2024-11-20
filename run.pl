2755899f4972505660f5d39a8f085411ad77548b
Jan Wielemaker <jan@swi-prolog.org>
2024-11-20 10:53:54 +0100

    Activate in_make/0

--- run.pl
+++ run.pl
@@ -1,3 +1,8 @@
 :- use_module(gui).
 :- ensure_loaded('../Garp3-v1.5.2/startup').
 :- use_module(map).
+
+:- if(exists_source(library(in_make))).
+:- use_module(library(in_make)).
+:- in_make.
+:- endif.
