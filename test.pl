372a2438a4d2574dc2a0602960934274514d895d
Jan Wielemaker <jan@swi-prolog.org>
2025-07-01 11:53:45 +0200

    Progress in testing.

--- test.pl
+++ test.pl
@@ -4,13 +4,17 @@
             run_test/2                  % ++File, +Options
           ]).
 :- use_module(library(filesex), [directory_file_path/3, make_directory_path/1]).
-:- use_module(dynalearn, [download_model/2]).
 :- use_module(library(ansi_term), [ansi_format/3]).
 :- use_module(library(apply), [convlist/3]).
 :- use_module(library(pprint), [print_term/2]).
 :- use_module(library(readutil), [read_file_to_terms/3]).
+:- use_module(library(statistics), [call_time/2]).
+:- use_module(library(http/json), [atom_json_dict/3]).
 
 :- use_module(equations).
+:- use_module(gsim).
+:- use_module(dynalearn,
+              [download_model/2, dynalearn_model/2, set_dynalearn_model/2]).
 :- use_module(map).
 :- use_module(diff).
 
@@ -38,8 +42,14 @@ several files:
 %!  save_test(+Test, +Data) is det.
 
 save_test(Test, Data) :-
-    download_model(Data.model, Dynalearn),
-    Data1 = Data.put(#{dynalearn:Dynalearn}),
+    Model = Data.model,
+    download_model(Model, DynalearnJSON),
+    dynalearn_model(Model, Dynalearn),
+    Data1 = Data.put(#{ dynalearn:
+                          #{ json: DynalearnJSON,
+                             prolog:Dynalearn
+                           }
+                      }),
     save_title(Data.model, Data.get(title,_)),
     test_data_file(Data.model, Test, File),
     setup_call_cleanup(
@@ -88,7 +98,7 @@ clean_extension(Ext, File, Base) :-
                 *******************************/
 
 :- meta_predicate
-    expect(0).
+    expect(+, 0).
 
 %!  run_test(++File, +Options)
 %
@@ -102,9 +112,25 @@ run_test(File, Options) :-
     test_gui_import(TestDict, Options).
 
 test_gui_import(TestDict, Options) :-
+    test_and_set_dynalearn_model(TestDict, Options),
     test_gui_model_import(TestDict, Options),
     test_gui_qspaces_import(TestDict, Options),
     test_simulate(TestDict, Options).
+
+%!  test_and_set_dynalearn_model(+TestDict, +Options)
+%
+%   Test the conversion of the Dynalearn JSON   document and set this as
+%   the current model.
+
+test_and_set_dynalearn_model(TestDict, _Options) :-
+    Model = TestDict.model,
+    atom_json_dict(TestDict.dynalearn.json, JSONDict,
+                   [ value_string_as(atom)
+                   ]),
+    dynalearn:import_model(JSONDict, PrologModel),
+    PrologModelExp = TestDict.dynalearn.prolog,
+    expect("Parse Dynalearn JSON", PrologModel =@= PrologModelExp),
+    set_dynalearn_model(Model, PrologModelExp).
 
 %!  test_gui_model_import(+TestDict, +Options)
 %
@@ -113,7 +139,7 @@ test_gui_import(TestDict, Options) :-
 test_gui_model_import(TestDict, _Options) :-
     latex_to_prolog_ex(TestDict.web_data.ml_source, Equations),
     Expected = TestDict.prolog_data.equations,
-    expect(Equations == Expected).
+    expect("Parse LaTeX equations", Equations == Expected).
 
 %!  test_gui_qspaces_import(+TestDict, +Options)
 %
@@ -125,13 +151,13 @@ test_gui_qspaces_import(TestDict, _Options) :-
     JQspaces = TestDict.web_data.jqspaces,
     QSpacesEx = TestDict.prolog_data.qspaces,
     gui:qspaces(Model, JQspaces, QSpaces),
-    expect(QSpaces == QSpacesEx).
+    expect("Parse quantity spaces", QSpaces == QSpacesEx).
 
 %!  test_simulate(+TestDict, +Options)
 %
 %   Run the simulation
 
-test_simulate(TestDict, Options) :-
+test_simulate(TestDict, _Options) :-
     Model = TestDict.model,
     #{ iterations:  Iterations,
        sample:      Sample,
@@ -149,21 +175,26 @@ test_simulate(TestDict, Options) :-
                    id_mapping(IdMapping),
                    qspaces(QSpaces),
                    equations(Equations)
-                 ].
+                 ],
+    call_time(simulate(terms(Equations), Series0, SimOptions), _Time),
+    init_derivatives(Series0, Series, IdMapping),
+    nq_series(Series, QSeries, [link_garp_states(true)|SimOptions]),
+    QSeriesExp = TestDict.results.qseries,
+    expect("Qualitative series", QSeries =@= QSeriesExp).
 
-%!  expect(:Goal)
+%!  expect(++Msg, :Goal)
 %
 %   Expect Goal to be true.  Similar   to  assertion/1,  but attempts to
 %   print failures in a more readable way.
 
-expect(Goal), call(Goal) =>
+expect(_Msg, Goal), call(Goal) =>
     true.
-expect(Goal) =>
-    unexpected(Goal).
+expect(Msg, Goal) =>
+    unexpected(Msg, Goal).
 
-unexpected(_:Goal) =>
-    unexpected(Goal).
-unexpected(V == E) =>
-    ansi_format(error, 'Unexpected result:~n', []),
+unexpected(Msg, _:Goal) =>
+    unexpected(Msg, Goal).
+unexpected(Msg, V == E) =>
+    ansi_format(error, '~s: unexpected result:~n', [Msg]),
     term_diff(V, E).
 
