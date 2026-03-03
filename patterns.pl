925beb30fefdaf438bf0e64b30138bddb5f3bda4
Jan Wielemaker <jan@swi-prolog.org>
2026-03-03 16:18:56 +0100

    Added cycle alignment

--- patterns.pl
+++ patterns.pl
@@ -1,9 +1,10 @@
 :- module(patterns,
           [ final_cycle/3,                      % ++List, -Cycle:list, -CycleCount
-            canonical_cycle/2                   % +Cycle0, -Cycle
+            canonical_cycle/2,                  % +Cycle0, -Cycle
+            align_cycles/4                      % +C1In, +C2In, -C1Out, -C2Out
           ]).
 :- autoload(library(lists), [reverse/2, append/3, min_member/2]).
-:- autoload(library(aggregate), [foldall/4]).
+:- autoload(library(aggregate), [foldall/4, aggregate_all/3]).
 
 /** <module> Various algorithms on lists (of states)
 */
@@ -73,3 +74,57 @@ std_min(A, B, Min) :-
 std_min_(<, A, _, A).
 std_min_(=, A, _, A).
 std_min_(>, _, A, A).
+
+%!  align_cycles(+C1In, +C2In, -C1Out, -C2Out) is det.
+%
+%   Align the longest of C1In and C2In such that as many as possible
+%   values are at the same position.
+
+align_cycles(C1, C2, C1a, C2a) :-
+    length(C1, L1),
+    length(C2, L2),
+    L1 >= L2,
+    !,
+    align_cycles_(C1, C2, C1a, C2a).
+align_cycles(C1, C2, C1a, C2a) :-
+    align_cycles_(C2, C1, C2a, C1a).
+
+align_cycles_(C1, C2, C1a, C2) :-
+    aggregate_all(max(Score, C1b),
+                  (  rotated_cycle(C1, C1b),
+                     match_count(C1b, C2, Score)
+                  ),
+                  max(_, C1a)).
+
+match_count(L1, L2, Count) :-
+    match_count(L1, L2, 0, Count).
+
+match_count([], _, Count, Count) :-
+    !.
+match_count(_, [], Count, Count) :-
+    !.
+match_count([H1|T1], [H2|T2], Count0, Count) :-
+    state_match(H1, H2, Match),
+    Count1 is Count0+Match,
+    match_count(T1, T2, Count1, Count).
+
+state_match(X, X, 1).
+state_match([X], X, 1).
+state_match(X, [X], 1).
+state_match(X, List, 0.5) :-
+    is_list(List),
+    memberchk(X, List).
+state_match(List, X, 0.5) :-
+    is_list(List),
+    memberchk(X, List).
+state_match(_, _, 0).
+
+
+%!  rotated_cycle(+List, -Rotated) is nondet.
+%
+%   Rotated is a rotation of List.  Generates |List| answers.
+
+rotated_cycle(List, Rotated) :-
+    append(Before, After, List),
+    After \= [],
+    append(After, Before, Rotated).
